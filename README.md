3c7f841587a9b824f404b8a27757e929671d444e
vlad-blana <vladbic@gmail.com>
2024-02-29 14:16:03 +0200

    More inputs

--- README.md
+++ README.md
@@ -18,3 +18,9 @@ The transaction executes ERC20 transfer between 2 addresses
 
 During my tests, some of them don't lock on debug_traceTransaction if the transfer is reversed from to to send with a valid amount.
 These are not my contracts and I don't know who deployed them. I don't know more than anyone could learn by using the explorer.
+
+There could be multiple issues so I've provided 4 instances this has happened.
+
+To test each of the 4 inputs (different from, to and data) just change the array index (values between 0..3) in the line 169
+
+`const input = inputs[0];`
--- anvilLockRepro.js
+++ anvilLockRepro.js
@@ -110,11 +110,13 @@ const doStuff = async (
   console.log('received txResponse');
   console.log();
 
+  console.log(txResponse);
+  console.log();
+
   console.log('sending evm_mine...');
   await anvilProvider.send('evm_mine', []);
   console.log('sent evm_mine');
   console.log();
-  console.log(txResponse);
 
   console.log('requesting tx receipt...');
   const txReceipt = await anvilProvider.getTransactionReceipt(txResponse.hash);
@@ -138,13 +140,33 @@ const doStuff = async (
 }
 
 
-const input = {
-  to: '0x0C2A894a9a279128a1b531bABdeC4F169518Ac2C',
-  from: '0xc7Fea9907F84388C599F311395774edc8332F82b',
-  data: '0xa9059cbb0000000000000000000000001538a3f30d65018bd3be1d744d82a6877936f44d0000000000000000000000000000000000000000605dc52d7a33b38dff157b3a'
-}
+const inputs = [
+  {
+    to: '0x0C2A894a9a279128a1b531bABdeC4F169518Ac2C',
+    from: '0xc7Fea9907F84388C599F311395774edc8332F82b',
+    data: '0xa9059cbb0000000000000000000000001538a3f30d65018bd3be1d744d82a6877936f44d0000000000000000000000000000000000000000605dc52d7a33b38dff157b3a'
+  },
+  {
+    to: '0x1B7B541BeA3aF39292FCe08649e4C4e1BEE408a1',
+    from: '0x57b329880e4FbfE5b58d078BD13D0DA30ce1Ef2B',
+    data: '0xa9059cbb00000000000000000000000057b329880e4fbfe5b58d078bd13d0da30ce1ef2b00000000000000000000000000000000000000003f4e7ac667ddf9bf7a8d1d27'
+  },
+  {
+    to: '0xB0D06895a957390145C6C6E0fbdB5d9dFbC21E18',
+    from: '0xAAf32C33b5C475F745A2E9CEd38c6453DFd75E8f',
+    data: '0xa9059cbb0000000000000000000000006200f994874ef517a00c479fbf3021ec511e8dfc000000000000000000000000000000000000000000000000065412c7cd1ce221'
+  },
+  {
+    to: '0x153f04aDCB55eA1b486212dA0C3dE7781f6DBC37',
+    from: '0x1dA27D81E4E43CD7445b33cf2B62CbA09878E842',
+    data: '0xa9059cbb000000000000000000000000227972286d58952931283620fcd3f0d1e5c9a73400000000000000000000000000000000000000000000000032a3cddea762f7be'
+  }
+]
 
 const blockNumber = 19735496;
+
+// CHANGE ONLY THE INDEX HERE
+const input = inputs[0];
 
 doStuff(
   'anvil.exe',
